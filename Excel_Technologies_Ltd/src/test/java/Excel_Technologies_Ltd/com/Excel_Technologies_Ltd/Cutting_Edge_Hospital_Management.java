ea9e10b7b4f88c91bbf4c0afc1d056796b7115df
Tanvir-Tain <49960242+Tanvir-Tain@users.noreply.github.com>
2024-05-28 14:35:46 +0600

    Add files via upload

--- Excel_Technologies_Ltd/src/test/java/Excel_Technologies_Ltd/com/Excel_Technologies_Ltd/Cutting_Edge_Hospital_Management.java
+++ Excel_Technologies_Ltd/src/test/java/Excel_Technologies_Ltd/com/Excel_Technologies_Ltd/Cutting_Edge_Hospital_Management.java
@@ -9,10 +9,12 @@ import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.ElementClickInterceptedException;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
-
+// https://github.com/Tanvir-Tain/Web_Automation
 
 public class Cutting_Edge_Hospital_Management extends BaseDriverEXCL {
 	
@@ -27,7 +29,6 @@ public class Cutting_Edge_Hospital_Management extends BaseDriverEXCL {
 	
 	driver.get(baseurlsignin);
 	driver.manage().window().maximize();
-	
 	driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
 	 WebElement usernamElement =driver.findElement(By.xpath("//input[@name=\"username\"]"));
 	 usernamElement.sendKeys("tester");
@@ -79,15 +80,42 @@ public class Cutting_Edge_Hospital_Management extends BaseDriverEXCL {
 		Thread.sleep(2000);
 		driver.findElement(By.xpath("//button[contains(text(),'Attend to Patient')]")).click();
 		Thread.sleep(2000);
-		//thum button
-		//driver.findElement(By.xpath("//button[contains(text(),'Skip')]")).click();
-		//driver.findElement(By.xpath("//body/div[@id='root']/div[2]/div[3]/div[2]/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/input[1]")).click();
-		//Thread.sleep(2000);
-		//driver.findElement(By.xpath("//span[contains(text(),'Save')]")).click();
-		//Thread.sleep(2000);
+		 try {
+			 
+			 if (driver.findElement(By.xpath("//button[contains(text(),'Skip')]")).isDisplayed())
+			 {
+				 driver.findElement(By.xpath("//button[contains(text(),'Skip')]")).click();
+					driver.findElement(By.xpath("//body/div[@id='root']/div[2]/div[3]/div[2]/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/input[1]")).click();
+					Thread.sleep(2000);
+					driver.findElement(By.xpath("//span[contains(text(),'Save')]")).click();
+					Thread.sleep(2000);
 
-		driver.navigate().forward();  // service-points
-		Thread.sleep(2000);
+					driver.navigate().forward();  // service-points
+					Thread.sleep(2000);
+			 }
+			 
+			 else {
+				 driver.navigate().forward();  // service-points
+					Thread.sleep(2000);
+			}
+		 }
+		 
+		 catch (Exception e) {
+			 driver.navigate().forward();  // service-points
+				Thread.sleep(2000);
+		}
+		
+		
+		
+//		//thum button
+//		driver.findElement(By.xpath("//button[contains(text(),'Skip')]")).click();
+//		driver.findElement(By.xpath("//body/div[@id='root']/div[2]/div[3]/div[2]/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/input[1]")).click();
+//		Thread.sleep(2000);
+//		driver.findElement(By.xpath("//span[contains(text(),'Save')]")).click();
+//		Thread.sleep(2000);
+//
+//		driver.navigate().forward();  // service-points
+//		Thread.sleep(2000);
 		
 		
 	}
@@ -111,49 +139,52 @@ public class Cutting_Edge_Hospital_Management extends BaseDriverEXCL {
 	while ((cell=reader.readNext())!=null)
 	{
 		for (int i=0 ; i<=cell.length ; i++) {
-			String weightString  = cell[i+3];
-			String HightString  = cell[i+4];
-			String TemperatureString  = cell[i+5];
-			String SystolicString  = cell[i+6];
-			String DiastolictString  = cell[i+7];
-			String PulseString  = cell[i+8];
-			String RespiratoryRateString  = cell[i+9];
-			String OxygenString  = cell[i+10]; 
-			
-			Thread.sleep(1000);
-			WebElement weightElement =driver.findElement(By.xpath("//input[@placeholder='Enter Weight (kg)']"));
-			weightElement.clear();
-			weightElement.sendKeys(weightString);
-			Thread.sleep(1000);
-			
-		WebElement highElement = driver.findElement(By.xpath("//input[@placeholder='Enter Height (cm)']"));
-			highElement.clear();
-			highElement.sendKeys(HightString);
-			
-			driver.findElement(By.xpath("//input[@placeholder='Enter Temperature (c)']")).sendKeys(TemperatureString);
-			Thread.sleep(1000);
-			
-			driver.findElement(By.xpath("//input[@placeholder='Enter Systolic (mmHg)']")).sendKeys(SystolicString);
-			Thread.sleep(1000);
-			
-			driver.findElement(By.xpath("//input[@placeholder='Enter Diastolic (mmHg)']")).sendKeys(DiastolictString);
-			Thread.sleep(1000);
-			
+				String weightString  = cell[i+3];
+				String HightString  = cell[i+4];
+				String TemperatureString  = cell[i+5];
+				String SystolicString  = cell[i+6];
+				String DiastolictString  = cell[i+7];
+				String PulseString  = cell[i+8];
+				String RespiratoryRateString  = cell[i+9];
+				String OxygenString  = cell[i+10]; 
+				
+				Thread.sleep(1000);
+				WebElement weightElement =driver.findElement(By.xpath("//input[@placeholder='Enter Weight (kg)']"));
+				weightElement.clear();
+				weightElement.sendKeys(weightString);
+				Thread.sleep(1000);
+				
+				WebElement highElement = driver.findElement(By.xpath("//input[@placeholder='Enter Height (cm)']"));
+				highElement.clear();
+				highElement.sendKeys(HightString);
+				
+				driver.findElement(By.xpath("//input[@placeholder='Enter Temperature (c)']")).sendKeys(TemperatureString);
+				Thread.sleep(1000);
+				
+				driver.findElement(By.xpath("//input[@placeholder='Enter Systolic (mmHg)']")).sendKeys(SystolicString);
+				Thread.sleep(1000);
+				
+				driver.findElement(By.xpath("//input[@placeholder='Enter Diastolic (mmHg)']")).sendKeys(DiastolictString);
+				Thread.sleep(1000);
+				
+				
+				driver.findElement(By.xpath("//input[@placeholder='Enter Pulse Rate (bpm)']")).sendKeys(PulseString);
+				Thread.sleep(1000);
+				driver.findElement(By.xpath("//input[@placeholder='Enter Respiratory Rate (bpm)']")).sendKeys(RespiratoryRateString);
+				Thread.sleep(1000);
+				Thread.sleep(1000);
+				driver.findElement(By.xpath("//span[contains(text(),'Save')]")).click();
+				break ;
+
 	
-			
-			driver.findElement(By.xpath("//input[@placeholder='Enter Pulse Rate (bpm)']")).sendKeys(PulseString);
-			Thread.sleep(1000);
-			driver.findElement(By.xpath("//input[@placeholder='Enter Respiratory Rate (bpm)']")).sendKeys(RespiratoryRateString);
-			Thread.sleep(1000);
-			Thread.sleep(1000);
-			driver.findElement(By.xpath("//span[contains(text(),'Save')]")).click();
-			break ;
 		}
 		Thread.sleep(2000);
 		driver.findElement(By.xpath("//body/div[@id='root']/div[2]/div[2]/div[1]/div[2]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/button[1]")).click();
 		Thread.sleep(2000);
 	
 	}
+	
+	
 	
 }
 	
